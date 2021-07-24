aa73079cfee4ddc795bf94c63a43f38a8f754ec9
hrlnsnchz <hrlnsnchz@gmail.com>
2021-07-24 13:49:44 -0500

    This chapter went over if, elif, and else statements.

---
+++ exercises/flow charts and basic flow chart concepts/else_example.py
@@ -0,0 +1,11 @@
+password = 'swordfish'
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
+
+password = 'shark'
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
---
+++ exercises/flow charts and basic flow chart concepts/if_example.py
@@ -0,0 +1,9 @@
+name = 'Alice'
+if name == 'Alice':
+    print('Hi Alice')
+print('Done')
+
+name = 'Bob'
+if name == 'Alice':
+    print('Hi Alice')
+print('Done')    
